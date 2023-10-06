e9e69aa33857ff5c6a97bbfcc605336b7e8c8799
m-amroune <amroune.moustapha@gmail.com>
2023-10-06 17:03:30 +0200

    test unitaire NewBill : handleChangeFile

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom";
+import { screen, waitFor, fireEvent } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
 import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
@@ -31,6 +31,41 @@ describe("Given I am connected as an employee", () => {
       const mailIcon = screen.getByTestId("icon-mail");
 
       expect(mailIcon.classList.contains("active-icon")).toBe(true);
+    });
+  });
+  describe("When I am on NewBill Page and I select a file", () => {
+    test("Then, the file is uploaded", () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({ type: "Employee", email: "a@a" })
+      );
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      const newBillPage = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage,
+      });
+
+      const handleChangeFile = jest.fn((e) => newBillPage.handleChangeFile(e));
+      const file = new File(["file.png"], "file.png", {
+        type: "image/png",
+      });
+      const inputFile = screen.getByTestId("file");
+      inputFile.addEventListener("change", handleChangeFile);
+      fireEvent.change(inputFile, {
+        target: { files: [file] },
+      });
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(inputFile.files[0].name).toBe("file.png");
     });
   });
 });
